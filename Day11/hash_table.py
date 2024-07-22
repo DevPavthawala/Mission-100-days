1764f6d700b887b6465a0af984df1e6b8b814e9d
DevPavthawala <devpavthawala6181@gmail.com>
2024-07-22 19:16:49 +0530

    Create hash_table.py
    customised array for work like dictionary

---
+++ Day11/hash_table.py
@@ -0,0 +1,48 @@
+# getting ascii value
+
+# def get_hash(key):
+#     h = 0
+#     for char in key:
+#         h += ord(char)
+#         return h % 100
+
+# print(get_hash('march 6'))
+# print(ord('m'))
+
+class HashTable:
+    def __init__(self):
+        self.Max =  100
+        self.arr = [None for i in range(self.Max)]
+
+    def get_hash(self,key):
+        h = 0
+        for char in key:
+            h += ord(char)
+            return h % self.Max
+
+    def __setitem__(self, key, value):
+        h = self.get_hash(key)
+        self.arr[h] = value
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+
+t = HashTable()
+
+t['april 6'] = 190
+t['april 25'] = 110
+t['march 1'] = 120
+t['dec 31'] = 130
+
+
+print(t.arr)
+
+del t['dec 31']
+
+print(t.arr)
